660a1feda1ba66c622d7fe7397861fa3f3bc8efd
Andrei <andreiflausino@live.com>
2024-05-16 17:47:27 -0300

    weatherapi working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+import weatherApi from "./weatherApi";
+
+const weather = await weatherApi("Milan");
+console.log(weather);
+
+async function userInput() {
+  const locationBtn = document.querySelector("#locationBtn");
+
+  locationBtn.addEventListener("click", async () => {
+    const userInput = document.querySelector("#location").value;
+    console.log(await weatherApi(userInput));
+  });
+}
+
+userInput();
---
+++ src/weatherApi.js
@@ -0,0 +1,21 @@
+export default async function weatherApi(location) {
+  const response = await fetch(
+    "http://api.weatherapi.com/v1/current.json?key=14d573de68f64a588bf221634241505&q=" + location,
+    {
+      mode: "cors",
+    }
+  );
+  const weatherData = await response.json();
+  const processedData = processApiData(weatherData);
+  return processedData;
+}
+
+function processApiData(data) {
+  const conditionText = data.current.condition.text;
+  const conditionIcon = data.current.condition.icon;
+  const celsius = data.current.temp_c;
+  const fahrenheit = data.current.temp_f;
+  const location = data.location.name;
+  const date = data.location.localtime;
+  return { conditionText, conditionIcon, celsius, fahrenheit, location, date };
+}
