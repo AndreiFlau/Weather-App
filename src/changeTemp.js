25cfbd952e37273fce5d7a8e68dd330dc2b28f95
Andrei <andreiflausino@live.com>
2024-05-23 18:46:55 -0300

    Initial dist subtree commit

--- src/changeTemp.js
+++
@@ -1,8 +0,0 @@
-export default function changeTemp(celsius, fahrenheit, elementValue, celsiusBtn, fahrenBtn) {
-  celsiusBtn.addEventListener("click", () => {
-    elementValue.textContent = celsius;
-  });
-  fahrenBtn.addEventListener("click", () => {
-    elementValue.textContent = fahrenheit;
-  });
-}
